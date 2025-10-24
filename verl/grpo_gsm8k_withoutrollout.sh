41ccce80cf547a75f5fd86097d560c0341793523
Yizhxy <1476672698@qq.com>
2025-10-24 02:30:13 +0000

    fix:gsm8k path in  grpo_gsm8k_withoutrollout.sh

--- verl/grpo_gsm8k_withoutrollout.sh
+++ verl/grpo_gsm8k_withoutrollout.sh
@@ -2,8 +2,8 @@ set -x
 
 CUDA_VISIBLE_DEVICES=0
 
-gsm8k_train_path=$HOME/data/gsm8k/train.parquet
-gsm8k_test_path=$HOME/data/gsm8k/test.parquet
+gsm8k_train_path=./data/gsm8k/train.parquet
+gsm8k_test_path=./data/gsm8k/test.parquet
 
 train_files="['$gsm8k_train_path']"
 test_files="['$gsm8k_test_path']"
